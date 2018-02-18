13f365f13c72473813b75c2f13149fd491284a7a
Evan Klitzke <evan@eklitzke.org>
2018-02-17 23:50:48 -0500

    finally figure out how to get ext4 mpage data

--- unpack.py
+++ unpack.py
@@ -6,16 +6,67 @@ import argparse
 import pickle
 import datetime
 from collections import defaultdict
-from typing import List, Dict, Set
+from typing import List, Dict, Set, Any, TextIO, Tuple
 
 import pandas as pd
-
-IGNORE_EVENTS = frozenset(['finish_ibd'])
 
 
 def arrange_fields(fields: Set[str]) -> List[str]:
     fields.remove('t')
     return ['t'] + list(sorted(fields))
+
+
+def split_fields(fields: List[str]) -> Dict[str, Any]:
+    info = {}
+    for field in fields:
+        k, v = field.split('=')
+        if k == 't':
+            info[k] = datetime.datetime.fromtimestamp(float(v))
+        else:
+            info[k] = int(v, 10)
+    return info
+
+
+def load_events(infile: TextIO) -> Tuple[List[datetime.datetime], Dict[
+        str, List[Dict[str, Any]]], Dict[str, List[str]]]:
+    data = defaultdict(list)
+    event_fields = defaultdict(set)
+    times = []
+    for line in infile:
+        fields = line.rstrip().split()
+        event = fields.pop(0)
+        info = split_fields(fields)
+
+        if event == 'time':
+            times.append(info['t'])
+        elif event == 'finish_ibd':
+            break
+        else:
+            data[event].append(info)
+
+        # track all the known fields
+        for k in info:
+            event_fields[event].add(k)
+
+    event_fields = {k: list(sorted(v)) for k, v in event_fields.items()}
+    return times, dict(data), event_fields
+
+
+def create_flushes_frame(flushes: List[Dict[str, Any]]) -> pd.DataFrame:
+    times = [flush['t'] for flush in flushes]
+    frame = pd.DataFrame(flushes, index=times)
+    columns = frame.columns.tolist()
+    columns.remove('t')
+    return frame[columns]
+
+
+def create_frame(times: List[datetime.datetime], data: List[Dict[str, Any]],
+                 columns: List[str]):
+    assert len(times) == len(data)
+    for row in data:
+        for col in columns:
+            row.setdefault(col, 0)
+    return pd.DataFrame(data, index=times)
 
 
 def main():
@@ -25,42 +76,14 @@ def main():
     parser.add_argument('filename')
     args = parser.parse_args()
 
-    events = defaultdict(list)
-    event_fields = defaultdict(set)
-
     with open(args.filename) as infile:
-        for line in infile:
-            fields = line.rstrip().split()
-            info = {}
-            event = None
-            for field in fields:
-                k, v = field.split('=')
-                if k == 't':
-                    info['t'] = datetime.datetime.fromtimestamp(float(v))
-                elif k == 'event':
-                    event = v
-                elif k == 'progress':
-                    info[k] = float(v)
-                else:
-                    info[k] = int(v, 10)
-            assert event
-            if event in IGNORE_EVENTS:
-                continue
-            for k in info:
-                event_fields[event].add(k)
-            events[event].append(info)
+        times, events, event_fields = load_events(infile)
 
     frames = {}
+    frames['flushes'] = create_flushes_frame(events.pop('flush'))
+
     for event, data in events.items():
-        fields = event_fields[event]
-        times = []
-        for row in data:
-            for field in fields:
-                row.setdefault(field, 0)
-            times.append(row['t'])
-        frame = pd.DataFrame(data, index=times)
-        fields.remove('t')
-        frames[event] = frame[sorted(fields)]
+        frames[event] = create_frame(times, data, event_fields[event])
 
     with open(args.output, 'wb') as outfile:
         save_data = {'filename': args.filename, 'frames': frames}
