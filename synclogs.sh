7cdc92f5b0f224c1a21e8c90e94a7b9fc15e6323
Evan Klitzke <evan@eklitzke.org>
2018-02-18 17:58:16 -0500

    make this more verbose

--- synclogs.sh
+++ synclogs.sh
@@ -2,10 +2,12 @@
 #
 # Sync remote and local logs
 
+set -eu
+
 if [ $# -ne 1 ]; then
   echo "usage: $0 HOST"
 fi
 
 pushd ~/logs &>/dev/null
-rsync "$1:logs/" .
+rsync -avz "$1:logs" .
 popd &>/dev/null
