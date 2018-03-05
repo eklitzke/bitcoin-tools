c87bc0bdb614cfab57e289616eb161c05cd99f7c
Evan Klitzke <evan@eklitzke.org>
2018-03-05 12:17:42 -0500

    make this better

--- synclogs.sh
+++ synclogs.sh
@@ -4,16 +4,44 @@
 
 set -eu
 
+VERBOSE=0
+while getopts ":vx" opt; do
+  case $opt in
+    v) VERBOSE=1 ;;
+    x) set -x ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+mkdirs() {
+  for d in ~/logs ~/.profiles; do
+    if [ ! -d "$d" ]; then
+      mkdir "$d"
+    fi
+  done
+}
+
+do_rsync() {
+  OPTS=(-az)
+  if [ "$VERBOSE" -eq 1 ]; then
+    OPTS+=(-v)
+  fi
+  rsync "${OPTS[@]}" "$@"
+}
+
 synchosts() {
-  if [ ! -d ~/logs ]; then
-    mkdir -p ~/logs
-  fi
-  if [ ! -d ~/.profiles ]; then
-    mkdir -p ~/.profiles
-  fi
+  mkdirs
   for h in "$@"; do
-    rsync -avz "$h:logs/" ~/logs/ &
-    rsync -avz "$h:.profiles/" ~/.profiles/ &
+    do_rsync "$h:logs/" ~/logs/ &
+    do_rsync "$h:.profiles/" ~/.profiles/ &
   done
   wait
 }
